93a8b810a4e03c0eea82813fdd63c835acb6040d
jeremyshapiro <jeremyshapiro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-06 18:53:55 +0000

    Version 0.2 added. Many radical changes to come soon
    git-svn-id: https://plugins.svn.wordpress.org/infusionsoft-affiliates/trunk@405630 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ options.php
@@ -0,0 +1,65 @@
+<div class="wrap">
+<h2>Infusionsoft Affiliates</h2>
+
+Please enter your Infusionsoft API Key below so that we can access affiliates from your system.
+
+<form method="post" action="options.php">
+<?php wp_nonce_field('update-options'); ?>
+<?php settings_fields('infusionsoftaffiliates'); ?>
+
+<table class="form-table">
+
+<tr valign="top">
+<th scope="row">Infusionsoft App Name:</th>
+<td><input type="text" name="infusionsoft_appname" value="<?php echo get_option('infusionsoft_appname'); ?>" size="8" />.Infusionsoft.com</td>
+</tr>
+
+
+<tr valign="top">
+<th scope="row">Infusionsoft API Key:</th>
+<td><input type="text" name="infusionsoft_apikey" value="<?php echo get_option('infusionsoft_apikey'); ?>" size="48" /></td>
+</tr>
+
+
+<tr valign="top">
+<th scope="row">Affiliate Caching:</th>
+<td>
+  <select name="affiliate_caching">
+    <option value="none" <?php if (get_option('affiliate_caching') == 'none') echo "selected=selected" ?>>Do Not Cache</option>
+    <option value="5"  <?php if (get_option('affiliate_caching') == '5') echo "selected=selected" ?> disabled>Refresh every 5 minutes</option>
+    <option value="60"  <?php if (get_option('affiliate_caching') == '60') echo "selected=selected" ?> disabled>Refresh Hourly</option>
+    <option value="3600"  <?php if (get_option('affiliate_caching') == '3600') echo "selected=selected" ?> disabled>Refresh Daily</option>
+  </select>
+</td>
+</tr>
+
+<tr valign="top">
+<th scope="row">Load Affiliates When:</th>
+<td>
+    <input type="radio" name="affiliate_load" value="request" <?php if (get_option('affiliate_load') == 'request') echo "checked" ?> disabled>
+	An affiliate code is in the param OR cookie <br/>
+    <input type="radio" name="affiliate_load" value="param"   <?php if (get_option('affiliate_load') == 'param') echo "checked" ?>>
+	An affiliate code is in just the param <br/>
+    <input type="radio" name="affiliate_load" value="cookie"  <?php if (get_option('affiliate_load') == 'cookie') echo "checked" ?>  disabled>
+	An affiliate code is in just the cookie <br/>
+    <input type="radio" name="affiliate_load" value="root"    ®<?php if (get_option('affiliate_load') == 'root') echo "checked" ?>    disabled>
+	An affiliate code is the root of the URL
+</td>
+</tr>
+
+<tr valign="top">
+<th scope="row">Affiliate Code Names:</th>
+<td><input type="text" name="affiliatecode_names" value="<?php echo get_option('affiliatecode_names'); ?>" size="48" /></td>
+</tr>
+
+
+</table>
+
+<input type="hidden" name="action" value="update" />
+
+<p class="submit">
+<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+
+</form>
+</div>
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Plugin Name ===
+Contributors: jeremyshapiro
+Tags: affiliates, affiliate program, infusion, infusionsoft, tracking, jeremy shapiro, webinar, webinars, personalization, caching
+Requires at least: 2.0.2
+Tested up to: 4.3
+Stable tag: 0.2
+
+This plugin allows you to load an Infusionsoft Affiliate's information into your wordpress pages using the [affiliate] shortcode.
+
+== Description ==
+
+After you configure your Infusionsoft Affiliate, you'll be able to sync up your affiliates to your wordpress plog and add in affiliate specific merge fields.
+
+Where might you use this?
+
+* Personalized sales pages for your affiliates to use in promoting your product
+* Personalized opt-in pages that appear to be written by your affiliate
+* Custom webinar sign up forms, one for each affiliate
+
+Use the [affiliate] shortcode with a specific field; for example, to get the affiliate's name, use [affiliate field="AffName"].
+
+You can also specify a default value like [affiliate field="AffName" default="your friend"].
+
+For fields that are dates, you can specify a [PHP friendly date format](http://php.net/manual/en/function.date.php), for example [affiliate field="_WebinarDate" format="l, f Js" /].
+
+All your custom affiliate fields get pulled down, too, just don't forget to put the underscore in front, i.e. _YourCustomField.
+
+== Installation ==
+
+To install the plugin:
+   1. Download the plugin
+   2. Upload to your wordpress blog
+   3. Activate and Configure
+
+= Configuration =
+
+To configure your installation, click Settings and then select enter your Infusionsoft Application name and API Key.
+
+== Updates ==
+
+7/6: Version 0.2 Added
